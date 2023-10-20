83ff73f298b0d4ce1a3853483d6798b4ae7120bb
AbhishekMahla <abhishekmahla22@gmail.com>
2023-10-20 13:48:06 +0530

    Feedback

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,4 +1,4 @@
-import { FaTimes} from 'react-icons/fa'
+import { FaTimes, FaEdit} from 'react-icons/fa'
 import { useContext } from "react";
 import Card from "./shared/Cards";
 import PropTypes from "prop-types";
@@ -7,7 +7,7 @@ import FeedbackContext from "./context/FeedbackContext";
         // {/* useing props */}
 // function FeedbackItem({ item , handleDelete}) {
   function FeedbackItem({ item }) {
-    const {deleteFeedback} = useContext(FeedbackContext)
+    const {deleteFeedback, editFeedback} = useContext(FeedbackContext)
   return (
     <>
       <Card>
@@ -16,6 +16,10 @@ import FeedbackContext from "./context/FeedbackContext";
         {/* <button onClick={()=>handleDelete(item.id)} className="close"> */}
         <button onClick={()=>deleteFeedback(item.id)} className="close"> 
           <FaTimes color='purple'/>
+          
+        </button>
+        <button onClick={()=>editFeedback(item)} className="edit">
+        <FaEdit color='purple'/>
         </button>
         <div className="text-display">{item.text}</div>
       </Card>
